e267d67cbb2fcc23c2917020780aff7c54b4dd63
Christopher Finke <cfinke@gmail.com>
2013-01-21 18:05:05 +0000

    Removing old header comment. It's all covered in the readme now.

--- pytivo_metadata.py
+++ pytivo_metadata.py
@@ -1,37 +1,5 @@
 #!/usr/bin/env python
 
-# When streaming videos to a TiVo with pyTivo, metadata files are used to show
-# metadata (duh) on the TiVo screens. Metadata files need to have the same
-# filename as the video file they go with, but with an additional .txt 
-# extension.
-#
-# If a video file has multiple audio tracks, this script generates an
-# additional hardlink to the video file with an audio override in the metadata
-# file that causes pyTivo to stream the commentary track instead of the 
-# original audio.  These additional files will have " - Commentary #1" (or #2,
-# or #3) appended to the title.  To take advantage of this, include this 
-# line in the [Server] section of your pyTivo.conf:
-#
-# audio_lang=commentary
-#
-# To denote multiple audio tracks, add " (Commentary)" (one commentary) or
-# " (Commentary x 2)" (two commentaries) to the video filename immediately 
-# before the extension. Example:
-#
-# The Family Man (Commentary x 3).m4v
-#
-# Yes, The Family Man, starring Nicolas Cage, does have three commentary tracks.
-#
-# This script was tested with the version of pyTivo from
-# https://github.com/wmcbrine/pytivo/commit/97c858d64726f9c6c1b117429c03a9f964411df1
-#
-# See http://pytivo.sourceforge.net/wiki/index.php/Metadata for more info
-# on the metadata file format.
-#
-# No guarantees are made regarding the correct movie being returned from the 
-# API, so maybe do a cursory check of that after running the script. Or don't. 
-# Whatever
-
 import sys
 import os
 import re
